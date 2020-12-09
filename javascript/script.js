62d38d18579aec2b0d1b9176d31e5241a8993174
Bryan Rainbow <bryanrbow@gmail.com>
2020-12-09 12:32:38 -0800

    Add code to hide form elements

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+
+function hide_form_elements() {
+    var label = document.getElementsByTagName("label")[0];
+    var input_area = document.getElementsByTagName("input")[0];
+    var button = document.getElementsByTagName("button")[0];
+
+    label.style.display = "none";
+    input_area.style.display = "none";
+    button.style.display = "none";
+}
